96a6e7ba3ec919cb65993ed3c85eaf43bb0542f4
cesar.jaquiery <cesar.jaquiery@students.fhnw.ch>
2024-05-01 13:50:07 +0200

    Modify AboutMe + Links

--- src/blog/AboutMe.tsx
+++ src/blog/AboutMe.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { Container, Grid, Paper, Box, Typography, CssBaseline, ThemeProvider, Button } from '@mui/material';
+import { Container, Grid, Paper, Box, Typography, CssBaseline, ThemeProvider, Button, Link } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import Layout from './Layout';
 import DownloadIcon from '@mui/icons-material/Download';
 
 const theme = createTheme();
+const practicalProjectURL = "https://studierendenprojekte.wirtschaft.fhnw.ch/view/2750"
+const bachelorProjectURL = "https://studierendenprojekte.wirtschaft.fhnw.ch/view/2019"
 
 export default function AboutMe() {
-
   return (
     <Layout>
       <main>
@@ -20,7 +21,7 @@ export default function AboutMe() {
               <Paper elevation={4} sx={{ p: 2, mb: 2 }}>
                 <img src={`${process.env.PUBLIC_URL}/assets/Passfoto.jpg`} alt="Image of myself" style={{ width: '100%', height: 'auto' }} />
               </Paper>
-              <Paper elevation={4} sx={{ p: 2 }}>
+              <Paper elevation={4} sx={{ p: 2, mb: 2 }}>
                 <Typography variant="h6" gutterBottom>Contact Details</Typography>
                 <Typography component="div" variant="body1" sx={{ display: 'flex', justifyContent: 'space-between' }}>
                   <span>Qualification:</span>
@@ -56,31 +57,52 @@ export default function AboutMe() {
                   Download CV
                 </Button>
               </Paper>
+              <Paper elevation={4} sx={{ p: 2, mb: 2 }}>
+                    <Typography variant="h6" gutterBottom>Interests</Typography>
+                    <Typography variant="body1"><ul>
+                        <li>Small private IT projects</li>
+                        <li>Climbing</li>
+                        <li>Hiking</li>
+                        <li>Water sports</li>
+                        <li>Diving</li>
+                      </ul></Typography>
+                  </Paper>
             </Grid>
             <Grid item xs={12} md={7}>
               <Paper elevation={4} sx={{ p: 2, mb: 2 }}>
                 <Typography variant="h6" gutterBottom>Hello, it's me César</Typography>
-                <Typography variant="body1">From a young age, I have been interested in the world of IT. 
-                My experience as a working student at a leading CDMO company helped me enhance my 
-                understanding of modern technologies and process optimization. I continue to broaden my 
-                knowledge by reading technical books, focusing on topics like Design Patterns, which 
-                deepens my interest in the field.<br />
+                <Typography variant="body1">From a young age, I have been fascinated by the world of IT. 
+                My experience as a working student at a renowned pharmaceutical company enhanced my 
+                understanding of modern technologies and process optimization, deepening my interest 
+                in the field. During my studies in Business Informatics, where I focused on programming 
+                modules, I actively engaged in practical projects with companies as clients, eagerly 
+                taking on responsibilities and applying theoretical knowledge in real-world settings.<br />
                   <br /></Typography>
-                <Typography variant="body1">My journey in software development is not just about
-                  acquiring knowledge; it's about applying what I learn to solve real-world problems.
-                  I'm constantly seeking opportunities to grow professionally and take on responsibilities
-                  in specific areas of expertise. I'm eager to work in dynamic environments that drive
-                  innovations, bringing my technical skills, creativity, and ability to work in a team
-                  to make a substantial impact.<br/>
+                <Typography variant="body1">This role allowed me to gain valuable experience and expand 
+                my understanding of various programming languages through script writing. It also 
+                deepened my interest in IT as I worked at the intersection of business and technology. 
+                During my civil service in IT support, I acquired comprehensive knowledge about diverse 
+                systems and their interactions, further solidifying my technical foundation. This 
+                experience was particularly enriching during a ransomware attack, which provided 
+                significant insights into how systems can interact and work together under crisis 
+                scenarios.<br/>
                   <br /></Typography>
-                <Typography variant="body1"> I'm excited about the possibilities that lie ahead and
-                  look forward to contributing to meaningful projects that make a difference. Let's
-                  embark on this journey together and explore how we can create innovative solutions
-                  that shape the future.</Typography>
+                <Typography variant="body1">I regularly engage in private projects to explore new 
+                technologies and consolidate my existing knowledge, like creating this webpage 
+                using AWS to understand its functionalities in practice. I always strive to extend 
+                my expertise by reading technical books, particularly on topics such as Design Patterns.
+                 My journey in software development is not just about acquiring knowledge; it's about 
+                 applying what I learn to solve real-world problems. I am constantly seeking 
+                 opportunities to grow professionally and take on responsibilities in specific areas of 
+                 expertise.<br/><br/></Typography>
+                 <Typography variant="body1">I am eager to work in dynamic environments that drive 
+                 innovation, bringing my technical skills, creativity, and team collaboration 
+                 abilities to make a substantial impact. I look forward to contributing to meaningful 
+                 projects that propel innovation and make a difference. Let's embark on this journey 
+                 together and explore how we can create innovative solutions that shape the 
+                 future.</Typography>
               </Paper>
-              <Grid container spacing={2}>
-                <Grid item xs={12} md={8}>
-                  <Paper elevation={4} sx={{ p: 2 }}>
+                  <Paper elevation={4} sx={{  p: 2, mb: 2 }}>
                     <Typography variant="h6" gutterBottom>Projects</Typography>
                     <Typography variant="subtitle2" gutterBottom>
                       Bachelor Thesis:
@@ -93,7 +115,10 @@ export default function AboutMe() {
                         <li>Research NFT technology for stronger customer loyalty in the company</li>
                         <li>Analyze and propose implementation options</li>
                         <li>Create a step-by-step implementation guide for the company</li>
-                      </ul><br /></Typography>
+                      </ul></Typography>
+                      <Link variant="body2" href={practicalProjectURL} target="_blank" sx={{zIndex: 2,}}>
+                      More details...
+                      </Link><br/><br/>
                     <Typography variant="subtitle2" gutterBottom>
                       Practical Project:
                     </Typography>
@@ -106,20 +131,11 @@ export default function AboutMe() {
                         <li>Carry out requirements analysis</li>
                         <li>Propose system improvements</li>
                       </ul></Typography>
+                      <Link variant="body2" href={bachelorProjectURL} target="_blank" sx={{zIndex: 2,}}>
+                      More details...
+                      </Link> 
                   </Paper>
-                </Grid>
                 <Grid item xs={12} md={4}>
-                  <Paper elevation={4} sx={{ p: 2 }}>
-                    <Typography variant="h6" gutterBottom>Interests</Typography>
-                    <Typography variant="body2">                      <ul>
-                        <li>Climbing</li>
-                        <li>Hiking</li>
-                        <li>Water sports</li>
-                        <li>Diving</li>
-                        <li>Motorbike</li>
-                      </ul></Typography>
-                  </Paper>
-                </Grid>
               </Grid>
             </Grid>
           </Grid>
--- src/blog/Imprint.tsx
+++ src/blog/Imprint.tsx
@@ -3,7 +3,6 @@ import { Container, Grid, Paper, Box, Typography, CssBaseline, ThemeProvider } f
 import { createTheme } from '@mui/material/styles';
 import Layout from './Layout';
 
-const theme = createTheme();
 
 export default function Imprint() {
   return (
@@ -14,49 +13,49 @@ export default function Imprint() {
         </Typography>
 
         <Box sx={{ my: 4 }}>
-          <Paper elevation={4} sx={{ p: 2, border: '2px solid', bgcolor: 'background.paper' }}>
-            <Typography variant="h6" gutterBottom sx={{ textDecoration: 'underline' }}>
+          <Paper elevation={4} sx={{ p: 2, bgcolor: 'background.paper' }}>
+            <Typography variant="subtitle1" gutterBottom sx={{ textDecoration: 'underline' }}>
               Purpose of the Website
             </Typography>
-            <Typography variant="subtitle1" paragraph sx={{ fontStyle: 'italic' }}>
+            <Typography variant="body2" paragraph sx={{ fontStyle: 'italic' }}>
             This website is designed to offer insights into the construction of the site itself and to introduce you to who I am. 
             </Typography>
           </Paper>
         </Box>
         <Box sx={{ my: 4 }}>
-          <Paper elevation={4} sx={{ p: 2, border: '2px solid', bgcolor: 'background.paper' }}>
-            <Typography variant="h6" gutterBottom sx={{ textDecoration: 'underline' }}>
+          <Paper elevation={4} sx={{ p: 2, bgcolor: 'background.paper' }}>
+            <Typography variant="subtitle1" gutterBottom sx={{ textDecoration: 'underline' }}>
               Contact Information
             </Typography>
             <Grid container spacing={2}>
             <Grid item xs={12} sm={3} md={2} lg={1}>
-                <Typography variant="subtitle1" paragraph sx={{ fontStyle: 'italic' }}>
+                <Typography variant="body2" paragraph sx={{ fontStyle: 'italic' }}>
                   Owner:
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={9} md={10} lg={11}>
-                <Typography variant="subtitle1" paragraph sx={{ fontStyle: 'italic' }}>
+                <Typography variant="body2" paragraph sx={{ fontStyle: 'italic' }}>
                   César Jaquiéry
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={3} md={2} lg={1}>
-                <Typography variant="subtitle1" paragraph sx={{ fontStyle: 'italic' }}>
+                <Typography variant="body2" paragraph sx={{ fontStyle: 'italic' }}>
                   Email:
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={9} md={10} lg={11}>
-                <Typography variant="subtitle1" paragraph sx={{ fontStyle: 'italic' }}>
+                <Typography variant="body2" paragraph sx={{ fontStyle: 'italic' }}>
                   c.jaquiery@gmail.com
                 </Typography>
               </Grid>
             </Grid>
           </Paper>
           <Box sx={{ my: 4 }}>
-            <Paper elevation={4} sx={{ p: 2, border: '2px solid', bgcolor: 'background.paper' }}>
-              <Typography variant="h6" gutterBottom sx={{ textDecoration: 'underline' }}>
+            <Paper elevation={4} sx={{ p: 2, bgcolor: 'background.paper' }}>
+              <Typography variant="subtitle1" gutterBottom sx={{ textDecoration: 'underline' }}>
                 Disclaimer
               </Typography>
-                  <Typography variant="subtitle1" paragraph sx={{ fontStyle: 'italic' }}>
+                  <Typography variant="body2" paragraph sx={{ fontStyle: 'italic' }}>
                   Information on this website is reviewed regularly but cannot guarantee its completeness, accuracy, or currentness.
                   </Typography>
             </Paper>
